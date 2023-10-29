5db4413e88ff01df4113ab9745253b6114860d3d
abdul-aziz <tamimaziz2007@gmail.com>
2023-10-29 18:26:36 +0600

    portfolio section

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    $("#basicExample").justifiedGallery({
+        rowHeight: 400,
+        margins: 20,
+        border: 0,
+        randomize: false,
+        waitThumbnailsLoad: false,
+    });
+    $("#basicExample2").justifiedGallery({
+        rowHeight: 400,
+        margins: 20,
+        border: 0,
+        randomize: false,
+        waitThumbnailsLoad: false,
+    });
+    $("#basicExample3").justifiedGallery({
+        rowHeight: 400,
+        margins: 20,
+        border: 0,
+        randomize: false,
+        waitThumbnailsLoad: false,
+    });
+});
